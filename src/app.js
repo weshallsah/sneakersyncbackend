341aede68abac8248f4d1e7768eb81d6d25afdf9
vishal <vishalk74064@gmail.com>
2024-07-14 12:24:36 +0530

    vercel json is added

--- src/app.js
+++ src/app.js
@@ -16,33 +16,10 @@ import wishlist from "./routes/wishlist.routes.js";
 import address from "./routes/Address.routes.js";
 import paymentroutes from "./routes/payment.routes.js"
 import path from "path";
-import Stripe from "stripe";
 
 
 app.get("/", async (req, res) => {
-    console.log("requrest is here");
-    const stripe = await Stripe(process.env.STRIPE_SECRET_KEY);
-    const testproduct = await stripe.products.create({
-        name: 'Starter Subscription',
-        description: 'INR 12/Month subscription',
-    }).then(async (product) => {
-        await stripe.prices.create({
-            unit_amount: 12,
-            currency: 'INR',
-            recurring: {
-                interval: 'month',
-            },
-            product: product.id,
-        }).then(price => {
-            console.log('product', product);
-            console.log("price ", price);
-        });
-    });
-    return res.status(200).json(
-        {
-            "message": "ok"
-        }
-    );
+    
 });
 app.get("/api/v1/", (req, res) => {
     return res.status(200).json({
